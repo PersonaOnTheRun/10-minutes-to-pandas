240e17f8bebddc2fe020be0fe2b25c599d4db526
PersonaOnTheRun <cammilligan@Camerons-MacBook-Pro.local>
2017-02-11 00:52:48 -0500

    new

---
+++ pandas learning.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 11 00:22:54 2017
+
+@author: cammilligan
+"""
+
+import pandas as pd
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+s = pd.Series([1,3,5,np.nan,6,8])
